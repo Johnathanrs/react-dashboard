1b6eb8689e0dc6534a53a57f3b92a57f8d5d35f7
johnathanrs <johnathans23@yahoo.com>
2015-12-04 14:00:21 +0200

    Host Performance API #2

--- server/methods/hosts.methods.js
+++ server/methods/hosts.methods.js
@@ -14,16 +14,22 @@ Meteor.methods({
 //            })
         
         
-        var containers = Containers.find({}, {limit: 100}).fetch();        
+        let containers = Containers.find({}, {limit: 100}).fetch();        
         return _.groupBy(containers, (c)=> c.DNSName);
     },
-    getHostSummary: (hostName)=>{
-        var containers = Containers.find({DNSName: hostName}).fetch();
+    getHostSummary: (hostName) => {
+        let containers = Containers.find({DNSName: hostName}).fetch();
         return {name: hostName, totalContainers: containers.length, ipAddress: containers[0].IPAddress, status: containers[0].Status};       
         
     },
-    getHost: (DNSName)=>{
-        var containers = Containers.find({DNSName: DNShostNameName}).fetch();
+    getHost: (hostName) => {
+        let containers = Containers.find({DNSName: hostName}).fetch();
         return {name: hostName, totalContainers: containers.length, ipAddress: containers[0].IPAddress, status: containers[0].Status, containers: containers};               
+    },	
+    getHostPerformance: (hostName) => {
+        let stat = ContainerStats.findOne({Host_DNS: hostName});
+        let  cpuUsage = Math.round(stat.cpu_stats.cpu_usage.percpu_usage[0] / stat.cpu_stats.cpu_usage.total_usage * 100) / 100;
+        let  memoryUsage = Math.round(stat.memory_stats.usage / stat.memory_stats.limit * 100) / 100;
+        return {name: hostName, cpuUsage: cpuUsage, memoryUsage: memoryUsage};               
     }
 })
