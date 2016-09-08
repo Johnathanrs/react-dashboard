7c3dc020cc3364e8c3c4cabc76790ce2250c0158
johnathanrs <johnathans23@yahoo.com>
2016-09-08 17:50:40 +0300

    cpuCard refactoring: gauge code reuse

--- latest/js/cpuCard.js
+++ latest/js/cpuCard.js
@@ -6,8 +6,7 @@ var letterSpacing = '1.40971704';
 var fontSize = '10.47494996';
 var pScale = d3.scaleLinear().domain([0, 100]).range([0, Math.PI * 2]);
 var rScale = d3.scaleLinear().domain([0, 100]).range([0, gaugeSize]);
-var r = (pScale(40));
-var p = Math.PI * 2;
+var fullArc = Math.PI * 2;
 
 d3.json("http://localhost:3000/api/container_stats/current/top5/cpu", function (error, data) {
   console.log("TEST executing dataviz function", data);
@@ -27,61 +26,63 @@ function dataViz(incomingData) {
   console.log("fifth element in incomingData's Name")
   console.log(incomingData[4].Names)
 
+  function buildGauge(gaugeIndex) {
+    d3.select("#cpu")
+      .select('.left' + (gaugeIndex+1) + ' h4')
+      .html(incomingData[gaugeIndex].Names.toString().substring(0, 16));
 
-  d3.select("#cpu").select('.left1 h4').html(incomingData[0].Names.toString().substring(0, 16))
+    var canvas = d3.select("#cpu").select('.right' + (gaugeIndex+1)).append("svg")
+      .attr("width", gaugeSize)
+      .attr("height", gaugeSize);
 
-  var canvas = d3.select("#cpu").select(".right1").append("svg")
-    .attr("width", gaugeSize)
-    .attr("height", gaugeSize);
+    var group = canvas.append("g")
+      .attr("transform", gaugeTransform);
 
-  var group = canvas.append("g")
-    .attr("transform", gaugeTransform);
+    var arcInner = d3.arc()
+      .innerRadius(rScale(40))
+      .outerRadius(rScale(48))
+      .startAngle(0)
+      .endAngle(pScale(Math.ceil(incomingData[gaugeIndex].percent)));
 
-  var arcInner = d3.arc()
-    .innerRadius(rScale(40))
-    .outerRadius(rScale(48))
-    //.innerRadius(10)
-    //.outerRadius(15)
-    .startAngle(0)
-    .endAngle(pScale(Math.ceil(incomingData[0].percent)))
+    var arcOuter = d3.arc()
+      .innerRadius(rScale(40))
+      .outerRadius(rScale(48))
+      .startAngle(0)
+      .endAngle(fullArc);
 
-  var arcOuter = d3.arc()
-    .innerRadius(rScale(40))
-    .outerRadius(rScale(48))
-    .startAngle(0)
-    .endAngle(p);
+    group.append("path")
+      .attr("d", arcOuter)
+      .attr("fill", gaugeBackColor);
 
-  group.append("path")
-    .attr("d", arcOuter)
-    .attr("fill", gaugeBackColor);
+    group.append("path")
+      .attr("d", arcInner)
+      .attr("fill", gaugeForeColor)
+      .style("opacity", 0.800000012);
 
-  group.append("path")
-    .attr("d", arcInner)
-    .attr("fill", gaugeForeColor)
-    .style("opacity", 0.800000012);
+    var text = group.selectAll("text")
+      .data([Math.ceil(incomingData[gaugeIndex].percent)])
+      .enter()
+      .append("text");
 
-  var text = group.selectAll("text")
-    .data([Math.ceil(incomingData[0].percent)])
-    .enter()
-    .append("text");
+    var textLabels = text
+      .attr("x", -6)
+      .attr("y", 4)
+      .text(function (d) {
+          return d + "%";
+      })
+      .attr("font-family", "San Francisco")
+      .attr("font-size", fontSize)
+      .attr("fill", "#4D4E57")
+      .attr("font-weight", "regular")
+      .attr("letter-spacing", letterSpacing);
+  }
 
-  var textLabels = text
-    .attr("x", -6)
-    .attr("y", 4)
-    .text(function (d) {
-      {
-        return d + "%"
-      }
-      ;
-    })
-    .attr("font-family", "San Francisco")
-    .attr("font-size", fontSize)
-    .attr("fill", "#4D4E57")
-    .attr("font-weight", "regular")
-    .attr("letter-spacing", letterSpacing)
+  [0, 1, 2, 3, 4].forEach(function (gaugeIndex) {
+    buildGauge(gaugeIndex);
+  });
 
 
-  d3.select("#cpu").select('.left2 h4').html(incomingData[1].Names.toString().substring(0, 16))
+  /*d3.select("#cpu").select('.left2 h4').html(incomingData[1].Names.toString().substring(0, 16))
   var canvas2 = d3.select("#cpu").select(".right2").append("svg")
     .attr("width", gaugeSize)
     .attr("height", gaugeSize);
@@ -279,8 +280,6 @@ function dataViz(incomingData) {
     .attr("fill", "#4D4E57")
     .attr("font-weight", "regular")
     .attr("letter-spacing", letterSpacing)
-
+*/
   d3.select("#svgdataurl").remove()
-
-
 }
