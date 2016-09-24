99c354964f4faae3c1e7a02cde83bc0c30755838
johnathanrs <johnathans23@yahoo.com>
2016-09-24 07:42:29 +0300

    Health API accessor stub

---
+++ latest/evoluteData/external/healthApi.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+const baseUrl = 'http://health-api.evolute.io:8500/v1/health/node/';
+
+// TODO get error counts
+
+function getSomething() {
+  return 'something';
+
+}
+
+module.exports = {};
+
