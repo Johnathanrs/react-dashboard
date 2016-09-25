83ab6d05dd6968501493eaea04438bbb8b72694f
johnathanrs <johnathans23@yahoo.com>
2016-09-25 12:07:02 +0300

    Container identification helper

---
+++ latest/evoluteData/helpers/containerIdentificationHelper.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+const mongoose = require('mongoose');
+
+const CurrentContainerInfo = require('../models/CurrentContainerInfo');
+
+let lxcIdToContainerName = {};
+let containerNameToLxcId = {};
+
+function initialize() {
+  return CurrentContainerInfo.aggregate([
+    {
+      $group: {
+        _id: '$lxc_id',
+        lxcId: {$first: '$lxc_id'},
+        containerName: {$first: '$Names'}
+      }
+    }
+  ]).then((items) => {
+    lxcIdToContainerName = {};
+    containerNameToLxcId = {};
+    items.forEach((item) => {
+      lxcIdToContainerName[item.lxcId] = item.containerName;
+      containerNameToLxcId[item.containerName] = item.lxcId;
+    });
+  });
+}
+
+function lxcIdByContainerName(containerName) {
+  return containerNameToLxcId[containerName];
+}
+
+function containerNameByLxcId(lxcId) {
+  return lxcIdToContainerName[lxcId];
+}
+
+module.exports = {
+  initialize,
+  lxcIdByContainerName,
+  containerNameByLxcId
+};
+
