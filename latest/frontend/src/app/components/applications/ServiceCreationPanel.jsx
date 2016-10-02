b9bd09793279bd11eae5738ce54dd469b1bdfbb4
johnathanrs <johnathans23@yahoo.com>
2016-10-02 12:17:07 +0300

    Code cleanup

--- latest/frontend/src/app/components/applications/ServiceCreationPanel.jsx
+++ latest/frontend/src/app/components/applications/ServiceCreationPanel.jsx
@@ -7,7 +7,6 @@ import TableColumn from '../common/table/TableColumn.jsx';
 
 import ErrorCount from './ErrorCount.jsx';
 import ApplicationSelectionSummary from './ApplicationSelectionSummary.jsx';
-//import ApplicationTable from './ApplicationTable.jsx';
 import ServiceCreationModal from './ServiceCreationModal.jsx';
 
 export default class ServiceCreationPanel extends React.Component {
@@ -55,14 +54,6 @@ export default class ServiceCreationPanel extends React.Component {
     </div>;
   }
 
-  /* TODO remove
-   <ApplicationTable ref="applicationTable"
-   items={ this.props.applications }
-   supportsSelection={ true }
-   onSelectionChange={ (selectedItems) => { this.setState({selectedApplicationCount: selectedItems.length})} }/>
-   */
-
-
   _showServiceCreationModal() {
     let modalHandle;
     const modalContent = <ServiceCreationModal applications={ this._selectedItems() }
@@ -78,12 +69,6 @@ export default class ServiceCreationPanel extends React.Component {
 
   onContinueServiceCreation() {
     this._showServiceCreationModal();
-
-    // TODO probably this is not needed
-    //const selectedItems = this._selectedItems();
-    //const applications = _.map(selectedItems, (item) => _.defaults({newInstanceCount: item.instanceCount}, item.application));
-
-    //this.props.onApply && this.props.onApply(applications);
   }
 
 }
