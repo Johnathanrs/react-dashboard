df21066c5c8971181a0fb7ab1cf0b55c1074ffab
johnathanrs <johnathans23@yahoo.com>
2016-11-22 02:49:50 +0300

    errorStatsExtractionTool updated - it supports HTTP querying now

--- latest/dataHandling/extraction/errorStatsExtractionTool.js
+++ latest/dataHandling/extraction/errorStatsExtractionTool.js
@@ -1,37 +1,131 @@
 const configuration = {
-  debug: false,
+  debug: true,
   mongo: {
     url: 'mongodb://localhost:27017/evolute',
     targetCollectionName: 'error_stats'
   },
+  currentPolicyName: 'http',
   loadPolicy: {
     fileSystem: {
       basePath: '/var/lib/docker/containers/',
       glob: '**/*-json.log'
+    },
+    http: {
+      healthApiBaseUrl: 'http://health-api.evolute.io:8500/v1/health/node/',
+      pollingIntervalMillis: 30000,
+      nodeListReloadIntervalMillis: 4000
     }
   }
 };
 
-var express = require('express');
-var app = express();
-const request = require('request');
+const axios = require('axios');
 const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 const q = require('q');
 const glob = require('glob');
+const moment = require('moment');
 const MongoClient = require('mongodb').MongoClient;
 
 
-
-
-const server = app.listen(3001);
+let mongoDatabase;
 
 // Import error matching rules
 const rules = require('./errorStatsExtractionTool.rules');
 
 const isErrorLine = (line) => _.some(rules, (rule) => rule(line));
+
+const normalizeContainerName = (containerName) => _.startsWith(containerName, '/') ? containerName.substring(1) : containerName;
+
+function initiateHttpPolling(options, listener) {
+  let nodeList = [];
+
+  /**
+   * Load container list
+   * @returns {*|promise}
+   */
+  function loadNodeList() {
+    const deferred = q.defer();
+    mongoDatabase.collection('current_container_stats').aggregate([{
+      $group: {
+        _id: '$container.id',
+        containerName: {
+          $push: '$container.name'
+        },
+        host: {
+          $push: '$host.node'
+        }
+      }
+    }, {
+      $project: {
+        _id: '$_id',
+        containerName: {
+          $arrayElemAt: ['$containerName', 0]
+        },
+        host: {
+          $arrayElemAt: ['$host', 0]
+        }
+      }
+    }]).toArray((error, result) => {
+      nodeList = result;
+      deferred.resolve(nodeList);
+    });
+    return deferred.promise;
+  }
+
+  /**
+   * Polls all the containers for error logs
+   */
+  function pollAllNodes() {
+    nodeList.forEach((node) => {
+      pollSingleNode(node);
+    });
+  }
+
+  /**
+   * Poll the given container for error longs and put pass the errors to listeners
+   * @param node
+   */
+  function pollSingleNode(node) {
+    const lxcId = node._id;
+    const containerName = normalizeContainerName(node.containerName);
+
+    // Get time 30 seconds before now
+    // TODO change to 30 seconds:
+    const since = moment().subtract(1, 'days').unix();
+
+    const url = `http://${node.host}:4243/containers/${containerName}/logs?stderr=1&stdout=1&timestamps=1&follow=0&since=${since}`;
+    configuration.debug && console.log('Querying ', url);
+    axios.get(url).then((response) => {
+      const logsAsMultilineText = response.data;
+      const lines = logsAsMultilineText.split(/\r\n|\r|\n/);
+      const matchedLines = _.filter(lines, (line) => isErrorLine(line));
+      configuration.debug && console.log(`For container ${containerName} matched lines are`, matchedLines);
+      listener.onContainerErrorsGot(matchedLines, lxcId, containerName);
+    }, (error) => {
+      console.log('ERROR', error);
+    });
+  }
+
+  function startApiPolling() {
+    setInterval(() => {
+      pollAllNodes();
+    }, options.pollingIntervalMillis);
+  }
+
+  function startNodeListReloading() {
+    setInterval(() => {
+      loadNodeList();
+    }, options.nodeListReloadIntervalMillis);
+  }
+
+  // Start node list reloading and error polling
+  startNodeListReloading();
+  startApiPolling();
+
+}
+
 
 const loadPolicies = {
   fileSystem: function (options, listener) {
@@ -56,7 +150,7 @@ const loadPolicies = {
         const logFileName = path.basename(filePath);
         const lxcId = logFileName.substring(0, logFileName.length - '-json.log'.length);
         readErrorsFromFile(filePath).then((errors) => {
-          listener.onContainerErrorsGot(lxcId, errors).then(() => {
+          listener.onContainerErrorsGot(errors, lxcId).then(() => {
             (++processedFileCount === files.length) && listener.onFinish();
           });
         });
@@ -65,21 +159,30 @@ const loadPolicies = {
     });
   },
   http: function (options, listener) {
-    // Implement later if necessary
+    initiateHttpPolling(options, listener);
   }
 };
 
-const loadPolicyName = () => _.keys(configuration.loadPolicy)[0];
+const loadPolicyName = () => configuration.currentPolicyName;
 
 function processErrors() {
   const deferred = q.defer();
   const policyName = loadPolicyName();
   const loadPolicy = loadPolicies[policyName];
   loadPolicy(configuration.loadPolicy[policyName], {
-    onContainerErrorsGot(lxcId, errors) {
+    onContainerErrorsGot(errors, lxcId, containerName) {
       const truncateTimeToMinutes = (time) => time.substring(0, 16);
-      const groupedErrorCounts = _.countBy(errors, (error) => truncateTimeToMinutes(error.time));
-      return storeErrorStatToMongo(lxcId, groupedErrorCounts);
+      const timeRegExp = /\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}/;
+      const extractTime = (error) => {
+        if (_.isObject(error)) {
+          return error.time;
+        } else {
+          const matches = error.match(timeRegExp);
+          return matches && matches.length > 0 ? matches[0] : '';
+        }
+      };
+      const groupedErrorCounts = _.countBy(errors, (error) => truncateTimeToMinutes(extractTime(error)));
+      return storeErrorStatToMongo(groupedErrorCounts, lxcId, containerName);
     },
     onFinish() {
       deferred.resolve({});
@@ -89,21 +192,24 @@ function processErrors() {
 }
 
 let targetMongoCollection;
-function storeErrorStatToMongo(lxcId, groupedErrorCounts) {
+function storeErrorStatToMongo(groupedErrorCounts, lxcId, containerName) {
   const deferred = q.defer();
-  const documentsToInsert = _.map(groupedErrorCounts, (errorCount, time) => ({errorCount, time, lxcId}));
+  const documentsToInsert = _.map(groupedErrorCounts, (errorCount, time) => ({errorCount, time, lxcId, containerName}));
   configuration.debug && console.log('Inserting... ', documentsToInsert);
   if (_.isEmpty(documentsToInsert)) {
     deferred.resolve();
   } else {
     targetMongoCollection.insertMany(documentsToInsert).then(() => {
       deferred.resolve();
+    }, (error) => {
+      console.log('MONGO ERROR', error);
     });
   }
   return deferred.promise;
 }
 
 MongoClient.connect(configuration.mongo.url).then((db) => {
+  mongoDatabase = db;
   return db.createCollection(configuration.mongo.targetCollectionName).then((targetCollection) => {
     targetMongoCollection = targetCollection;
     processErrors().then(() => {
--- latest/dataHandling/extraction/errorStatsExtractionTool.rules.js
+++ latest/dataHandling/extraction/errorStatsExtractionTool.rules.js
@@ -1,6 +1,8 @@
 const errorWordRegexp = new RegExp('\\Werror\\W', 'i');
+const endpointWordRegexp = new RegExp('endpoint', 'i');
 
 module.exports = [
   (s) => errorWordRegexp.test(s),
-  (s) => false
+  (s) => false,
+  (s) => endpointWordRegexp.test(s)
 ];
